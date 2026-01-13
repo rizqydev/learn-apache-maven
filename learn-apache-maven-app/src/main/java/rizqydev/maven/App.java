f85b25152d2f31b714b0276aeb1e446d8e9e728d
Rizqy <rizqy.dev@gmail.com>
2026-01-13 08:28:12 +0700

    maven multi module project

---
+++ learn-apache-maven-app/src/main/java/rizqydev/maven/App.java
@@ -0,0 +1,16 @@
+package rizqydev.maven;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        Person person = new Person("Rizqy Dev");
+        String json = gson.toJson(person);
+
+        System.out.println("Person JSON: " + json);
+    }
+}
